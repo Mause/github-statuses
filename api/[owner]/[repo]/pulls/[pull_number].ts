f32e934f966cd61656a862a8bfe7e1c940f5aa20
Elliana May <me@mause.me>
2023-02-15 15:52:56 +0800

    Update and rename [id].ts to [pull_number].ts

---
+++ api/[owner]/[repo]/pulls/[pull_number].ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const octokit = new Octokit();
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse,
+) {
+  const pr = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', request.query)
+  const statuses = await octokit.request('GET /repos/{owner}/{repo}/commits/{ref}/statuses', {
+    ...request.query,
+    ref: pr.head.sha
+  });
+
+  response.status(200).json({
+    body: request.body,
+    query: request.query,
+    cookies: request.cookies,
+    statuses,
+  });
+}
