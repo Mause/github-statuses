470d0e0eb2a572da5743563e5ac89437b21030b9
Elliana May <me@mause.me>
2023-02-15 19:52:35 +0800

    integrate

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,23 +1,23 @@
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { Octokit } from "@octokit/rest";
+import type { LoaderArgs } from "@remix-run/node";
 
-export const loader = async () => {
-  return json({
-    posts: [
-      {
-        slug: "my-first-post",
-        title: "My First Post",
-      },
-      {
-        slug: "90s-mixtape",
-        title: "A Mixtape I Made Just For You",
-      },
-    ],
-  });
+const octokit = new Octokit();
+
+export const loader = async ({ params }: LoaderArgs) => {
+  const pr = await octokit.rest.pulls.get(params);
+  const statuses = (await octokit.rest.checks.listForRef({
+    ...params,
+    ref: pr.data.head.sha
+  })).data.check_runs.filter(status => status.conclusion !== 'success');
+  console.log(statuses);
+
+  return json({statuses});
 };
 
 export default function Index() {
-  const { posts } = useLoaderData<typeof loader>();
+  const { statuses } = useLoaderData<typeof loader>();
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
@@ -42,11 +42,10 @@ export default function Index() {
           </a>
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
+            Statuses: { statuses.length }
         </li>
       </ul>
     </div>
   );
 }
+
