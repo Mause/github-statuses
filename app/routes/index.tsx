281793ea6ccfc4a464235a66ecfbfdac67b7f287
Elliana May <me@mause.me>
2023-02-15 19:45:25 +0800

    dummy data

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,24 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+  return json({
+    posts: [
+      {
+        slug: "my-first-post",
+        title: "My First Post",
+      },
+      {
+        slug: "90s-mixtape",
+        title: "A Mixtape I Made Just For You",
+      },
+    ],
+  });
+};
+
 export default function Index() {
+  const { posts } = useLoaderData<typeof loader>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
