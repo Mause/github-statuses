e251f1e2f6f8988d4f8bec22abea6fb54f98005b
Elliana May <me@mause.me>
2023-02-15 15:38:47 +0800

    Update [id].ts

--- api/[owner]/[repo]/pulls/[id].ts
+++ api/[owner]/[repo]/pulls/[id].ts
@@ -1,3 +1,12 @@
-export default function (req, res) => {
-  res.json({hello: 'world'});
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(
+  request: NextApiRequest,
+  response: NextApiResponse,
+) {
+  response.status(200).json({
+    body: request.body,
+    query: request.query,
+    cookies: request.cookies,
+  });
 }
