1e4f6ccd22fd8a8787b6c8fd88236831de0f14b7
Elliana May <me@mause.me>
2023-02-16 08:07:10 +0800

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,10 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to GitHub Statuses</h1>
       <ul>
         <li>
-          <a href="/duckdb/duckdb/pulls/1000">
+          <a href="/duckdb/duckdb/pull/1000">
             Quickstart
           </a>
         </li>
