5092dbddc5523f2341105524c13de56985fae9a5
Elliana May <me@mause.me>
2023-02-18 19:22:37 +0800

    feat: add throttling

--- app/octokit.server.ts
+++ app/octokit.server.ts
@@ -1,5 +1,6 @@
 import { Octokit } from "@octokit/rest";
 import { createAppAuth, StrategyOptions } from "@octokit/auth-app";
+import { throttling } from "@octokit/plugin-throttling";
 
 const auth: StrategyOptions = {
   appId: process.env.GITHUB_APP_ID!,
@@ -8,7 +9,43 @@ const auth: StrategyOptions = {
   clientSecret: process.env.GITHUB_SECRET!,
   installationId: process.env.GITHUB_INSTALL_ID,
 };
-export const octokit = new Octokit({
+
+const Throttled = Octokit.plugin(throttling);
+
+interface Request {
+  method: string;
+  url: string;
+}
+
+export const octokit = new Throttled({
   authStrategy: createAppAuth,
   auth,
+  throttle: {
+    onRateLimit: (
+      retryAfter: number,
+      options: Request,
+      octokit: Octokit,
+      retryCount: number
+    ) => {
+      octokit.log.warn(
+        `Request quota exhausted for request ${options.method} ${options.url}`
+      );
+
+      if (retryCount < 1) {
+        // only retries once
+        octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+        return true;
+      }
+    },
+    onSecondaryRateLimit: (
+      retryAfter: any,
+      options: { method: any; url: any },
+      octokit: Octokit
+    ) => {
+      // does not retry, only logs a warning
+      octokit.log.warn(
+        `SecondaryRateLimit detected for request ${options.method} ${options.url}`
+      );
+    },
+  },
 });
