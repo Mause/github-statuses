cf39903e628f867a5dd0cdb508200a3aac74ffff
Elliana May <me@mause.me>
2023-02-21 02:08:24 +0000

    add style to page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
-import { Form } from "react-bulma-components";
+import { Container, Form } from "react-bulma-components";
 import { useState } from "react";
 import { useNavigate } from "@remix-run/react";
-import { Spinner } from "@primer/react";
+import { Box, Header, Spinner } from "@primer/react";
 
 export default function Index() {
   const [value, set] = useState<string>();
@@ -9,26 +9,43 @@ export default function Index() {
   const navigate = useNavigate();
 
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to GitHub Statuses</h1>
-      <ul>
-        <li>
-          <a href="/duckdb/duckdb/pull/1000">Quickstart</a>
-        </li>
-        <li>{loading && <Spinner />}</li>
-        <li>
-          <form
-            onSubmit={(event) => {
-              event.preventDefault();
-              setLoading(true);
-              navigate(new URL(value!).pathname);
-            }}
-          >
-            <Form.Input onChange={(event) => set(event.target.value)} />
-            <Form.Input type="submit" disabled={loading} />
-          </form>
-        </li>
-      </ul>
-    </div>
+    <Container style={{ padding: "10px" }}>
+      <Box
+        sx={{
+          overflowY: "auto",
+          border: "1px solid",
+          borderRadius: "6px",
+          borderColor: "border.default",
+        }}
+      >
+        {" "}
+        <Header>
+          <Header.Item>
+            <Header.Link href="#">Action Statuses</Header.Link>
+          </Header.Item>
+          <Header.Item>
+            <Header.Link href="/duckdb/duckdb/pull/1000">
+              Quickstart
+            </Header.Link>
+          </Header.Item>
+        </Header>
+        <ul>
+          <li>https://github.com/Mause/duckdb/pull/27</li>
+          <li>{loading && <Spinner />}</li>
+          <li>
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+                setLoading(true);
+                navigate(new URL(value!).pathname);
+              }}
+            >
+              <Form.Input onChange={(event) => set(event.target.value)} />
+              <Form.Input type="submit" disabled={loading} />
+            </form>
+          </li>
+        </ul>
+      </Box>
+    </Container>
   );
 }
