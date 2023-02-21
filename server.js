49f36e46b40049adcfaf6d2230d955b26d61c3ee
Elliana May <me@mause.me>
2023-02-21 06:33:31 +0000

    add dummy getLoadContext

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 import { createRequestHandler } from "@remix-run/vercel";
 import * as build from "@remix-run/dev/server-build";
 
-export default createRequestHandler({ build, mode: process.env.NODE_ENV });
+export default createRequestHandler({
+  build,
+  mode: process.env.NODE_ENV,
+  getLoadContext() {
+    console.log("loading context");
+    return {};
+  },
+});
