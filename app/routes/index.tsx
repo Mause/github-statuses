26abe39ec8debffdeeefb9e8f490d664e9fd24b4
Elliana May <me@mause.me>
2023-02-21 02:08:00 +0000

    add loading spinner and submit button

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,11 @@
 import { Form } from "react-bulma-components";
 import { useState } from "react";
 import { useNavigate } from "@remix-run/react";
+import { Spinner } from "@primer/react";
 
 export default function Index() {
   const [value, set] = useState<string>();
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   return (
@@ -13,15 +15,17 @@ export default function Index() {
         <li>
           <a href="/duckdb/duckdb/pull/1000">Quickstart</a>
         </li>
+        <li>{loading && <Spinner />}</li>
         <li>
           <form
             onSubmit={(event) => {
               event.preventDefault();
+              setLoading(true);
               navigate(new URL(value!).pathname);
             }}
           >
             <Form.Input onChange={(event) => set(event.target.value)} />
-            https://github.com/Mause/duckdb/pull/27
+            <Form.Input type="submit" disabled={loading} />
           </form>
         </li>
       </ul>
