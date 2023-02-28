996ef0557442745b5ef8768a58313c31e29d9348
Elliana May <me@mause.me>
2023-02-28 06:47:17 +0000

    feat: add tree based home

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,30 +1,53 @@
 import { Form } from "react-bulma-components";
 import { useState } from "react";
-import { useNavigate } from "@remix-run/react";
-import { Spinner } from "@primer/react";
+import { Link, useNavigate } from "@remix-run/react";
+import { Box, Spinner, TreeView } from "@primer/react";
 import { Wrapper } from "~/components";
 
+const REPOS: [string, string[]][] = [
+  [
+    "Mause",
+    [
+      "duckdb",
+      "duckdb-web",
+      "duckdb_engine",
+      "mause.github.com",
+      "github-statuses",
+    ],
+  ],
+  ["duckdb", ["duckdb", "duckdb-web"]],
+  ["duckdblabs", ["sqlite_scanner", "substrait", "postgres_scanner"]],
+];
 export default function Index() {
   const [value, set] = useState<string>();
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
+  const nodes = REPOS.map(([owner, subs]) => (
+    <TreeView.Item id={owner} key={owner} defaultExpanded={true}>
+      {owner}
+      <TreeView.SubTree>
+        {subs.map((sub) => {
+          const id = `${owner}/${sub}`;
+          const href = `/${id}/pulls`;
+          return (
+            <TreeView.Item key={id} id={id} onSelect={() => navigate(href)}>
+              <Link to={href}>{sub}</Link>
+            </TreeView.Item>
+          );
+        })}
+      </TreeView.SubTree>
+    </TreeView.Item>
+  ));
+
   return (
     <Wrapper>
       {<></>}
       {
-        <ul>
-          <li>
-            <a href="/duckdb/duckdb/pulls">duckdb/duckdb</a>
-          </li>
-          <li>
-            <a href="/Mause/duckdb/pulls">Mause/duckdb</a>
-          </li>
-          <li>
-            <a href="/Mause/duckdb_engine/pulls">Mause/duckdb_engine</a>
-          </li>
-          <li>{loading && <Spinner />}</li>
-          <li>
+        <>
+          <TreeView>{nodes}</TreeView>
+          <Box>
+            {loading && <Spinner />}
             <form
               onSubmit={(event) => {
                 event.preventDefault();
@@ -35,8 +58,8 @@ export default function Index() {
               <Form.Input onChange={(event) => set(event.target.value)} />
               <Form.Input type="submit" disabled={loading} />
             </form>
-          </li>
-        </ul>
+          </Box>
+        </>
       }
     </Wrapper>
   );
