546b46bf03a44e04542c7c1d9b3df6934dec92a5
Elliana May <me@mause.me>
2023-03-02 12:24:54 +0800

    feat: auth

--- app/octokit.server.ts
+++ app/octokit.server.ts
@@ -1,6 +1,9 @@
 import { Octokit } from "@octokit/rest";
-import { createAppAuth, StrategyOptions } from "@octokit/auth-app";
+import type { StrategyOptions } from "@octokit/auth-app";
+import { createAppAuth } from "@octokit/auth-app";
 import { throttling } from "@octokit/plugin-throttling";
+
+import { GitHubStrategy } from "remix-auth-github";
 
 const auth: StrategyOptions = {
   appId: process.env.GITHUB_APP_ID!,
@@ -49,3 +52,17 @@ export const octokit = new Throttled({
     },
   },
 });
+
+let gitHubStrategy = new GitHubStrategy(
+  {
+    clientID: "YOUR_CLIENT_ID",
+    clientSecret: "YOUR_CLIENT_SECRET",
+    callbackURL: "https://example.com/auth/github/callback",
+  },
+  async ({ accessToken, extraParams, profile }) => {
+    // Get the user data from your DB or API using the tokens and profile
+    return User.findOrCreate({ email: profile.emails[0].value });
+  }
+);
+
+authenticator.use(gitHubStrategy);
---
+++ app/routes/auth/github/callback.tsx
@@ -0,0 +1,10 @@
+// app/routes/auth/github/callback.tsx
+import type { LoaderArgs } from "@remix-run/node";
+import { authenticator } from "~/services/auth.server";
+
+export async function loader({ request }: LoaderArgs) {
+  return authenticator.authenticate("github", request, {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+  });
+}
