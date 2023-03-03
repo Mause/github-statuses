11b21c0a4d32e549f01e33aa24c1af8204a84f9a
Elliana May <me@mause.me>
2023-03-03 02:56:23 +0000

    feat: add basic logout page

---
+++ app/routes/auth/logout.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@primer/react";
+import type { ActionArgs } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+import { Wrapper } from "~/components";
+import { authenticator } from "~/services/auth.server";
+
+export const action = async ({ request }: ActionArgs) => {
+  return authenticator.logout(request, {
+    redirectTo: "/login",
+  });
+};
+
+export default function Logout() {
+  return (
+    <Wrapper>
+      {<></>}
+      <Form method="post">
+        <Button type="submit">Logout with GitHub</Button>
+      </Form>
+    </Wrapper>
+  );
+}
