4b85dc2f6b3065cab52713e50083710096cf22f7
Elliana May <me@mause.me>
2023-03-13 07:00:21 +0000

    refactor: switch pull_number to graphql

--- app/routes/$owner/$repo/pull/$pull_number.tsx
+++ app/routes/$owner/$repo/pull/$pull_number.tsx
@@ -1,4 +1,8 @@
-import type { MetaFunction, TypedResponse } from "@remix-run/node";
+import type {
+  MetaFunction,
+  SerializeFrom,
+  TypedResponse,
+} from "@remix-run/node";
 import { json } from "@remix-run/node";
 
 import { useRevalidator } from "@remix-run/react";
@@ -18,8 +22,6 @@ import {
   LinkExternalIcon,
 } from "@primer/octicons-react";
 import { Header, Spinner, StyledOcticon, Flash } from "@primer/react";
-import { getOctokit } from "~/octokit.server";
-import { getWorkflowName } from "./getWorkflowName";
 import humanizeDuration from "humanize-duration";
 import type { DataLoaderParams } from "~/components/index";
 import { StandardTable, Wrapper } from "~/components/index";
@@ -27,6 +29,9 @@ import type { StandardTableOptions } from "~/components/StandardTable";
 import { countBy } from "lodash";
 import { titleCase } from "./titleCase";
 import { useLoaderDataReloading } from "~/components/useRevalidateOnFocus";
+import { getActions } from "./pullNumberQuery";
+import type { Get } from "type-fest";
+import type { PullRequestsFragment } from "~/components/graphql/graphql";
 
 export const meta: MetaFunction = ({ data }) => ({
   title: (data?.pr ? `${data?.pr?.title} | ` : "") + "Action Statuses",
@@ -38,53 +43,33 @@ export const loader = async ({
 }: DataLoaderParams<"repo" | "owner" | "pull_number">): Promise<
   TypedResponse<ReturnShape>
 > => {
-  const args = {
+  const pr = await getActions(request, {
     repo: params.repo!,
     owner: params.owner!,
-    pull_number: Number(params.pull_number!),
-  };
+    prNumber: Number(params.pull_number!),
+  });
+  const statuses = pr.commits!.nodes![0]?.commit!.checkSuites!.nodes!;
 
-  const octokitI = await getOctokit(request);
-  const pr = await octokitI.rest.pulls.get(args);
-  if (pr.status !== 200) {
-    throw new Error(JSON.stringify(pr.data));
-  }
-  const statuses = await octokitI.paginate(
-    octokitI.rest.checks.listForRef,
-    {
-      ...args,
-      ref: pr.data.head.sha,
-    },
-    (response) => {
-      if (response.status !== 200) {
-        throw new Error(JSON.stringify(response.data));
-      }
-      return response.data;
-    }
-  );
+  const augmentedStatuses = statuses
+    .filter((status) => !["success", "skipped"].includes(status!.conclusion!))
+    .flatMap((status): Item[] => {
+      const workflowName =
+        status!.workflowRun?.workflow?.name ?? status!.app!.name!;
 
-  const augmentedStatuses = await Promise.all(
-    statuses
-      .filter((status) => !["success", "skipped"].includes(status.conclusion!))
-      .map(async (status: Check): Promise<Item> => {
-        const started_at = Date.parse(status.started_at!);
-        const poi = Date.parse(status.completed_at!) || Date.now();
+      return status!.checkRuns!.nodes!.map((node) => {
+        const started_at = Date.parse(node!.startedAt!);
+        const poi = Date.parse(node!.completedAt!) || Date.now();
 
-        const run_id = getRunId(status);
-        const workflowName = run_id
-          ? await getWorkflowName(octokitI, params.owner!, params.repo!, run_id)
-          : status.app!.name!;
-
-        return Object.assign(status, {
+        return Object.assign({}, node, {
           workflowName,
           duration: poi - started_at,
         });
-      })
-  );
+      });
+    });
 
   return json({
     statuses: augmentedStatuses,
-    pr: pr.data,
+    pr,
     progress: Math.round(
       100 - (augmentedStatuses.length / statuses.length) * 100
     ),
@@ -93,43 +78,48 @@ export const loader = async ({
 
 type PR = Awaited<ReturnType<Octokit["rest"]["pulls"]["get"]>>["data"];
 
-type Check = Awaited<
-  ReturnType<Octokit["rest"]["checks"]["listForRef"]>
->["data"]["check_runs"][0];
+type Check = NonNullable<
+  Get<
+    PullRequestsFragment,
+    "pullRequest.commits.nodes.0.commit.checkSuites.nodes.0.checkRuns.nodes.0"
+  >
+>;
 
 type Conclusion = Check["conclusion"];
 type Status = Check["status"];
 
 type Item = Check & { workflowName: string; duration: number };
-type ReturnShape = { statuses: Item[]; pr: PR; progress: number };
+type ReturnShape = {
+  statuses: Item[];
+  pr: PullRequestsFragment["pullRequest"];
+  progress: number;
+};
 
-const columnHelper = createColumnHelper<Item>();
-
-function getRunId(status: Check): number | undefined {
-  const details_url = status.details_url!;
-
-  const match = /runs\/(\d+)\/jobs/.exec(details_url);
-
-  return match ? Number(match[1]) : undefined;
-}
+const columnHelper = createColumnHelper<SerializeFrom<Item>>();
 
 const color = (component: Icon, color: string) => () =>
   <StyledOcticon icon={component} color={color} />;
 
 const iconMap: Record<NonNullable<Conclusion | Status>, Icon> = {
-  success: color(CheckIcon, "success.fg"),
-  failure: color(XIcon, "danger.fg"),
+  SUCCESS: color(CheckIcon, "success.fg"),
+  FAILURE: color(XIcon, "danger.fg"),
 
-  skipped: SkipIcon,
-  cancelled: StopIcon,
+  SKIPPED: SkipIcon,
+  CANCELLED: StopIcon,
 
   // guesses
-  action_required: XIcon,
-  neutral: QuestionIcon,
-  timed_out: color(ClockIcon, "danger.fg"),
-  in_progress: color(DotIcon, "attention.fg"),
-  completed: XIcon,
-  queued: HourglassIcon,
+  ACTION_REQUIRED: XIcon,
+  NEUTRAL: QuestionIcon,
+  TIMED_OUT: color(ClockIcon, "danger.fg"),
+  IN_PROGRESS: color(DotIcon, "attention.fg"),
+  COMPLETED: XIcon,
+  QUEUED: HourglassIcon,
+
+  STALE: QuestionIcon,
+  PENDING: QuestionIcon,
+  REQUESTED: QuestionIcon,
+  STARTUP_FAILURE: QuestionIcon,
+  WAITING: QuestionIcon,
 };
 
 const COLUMNS = [
@@ -139,7 +129,7 @@ const COLUMNS = [
   columnHelper.accessor("name", {
     header: "Job Name",
     cell: (props) => (
-      <a target="_blank" href={props.row.original.html_url!} rel="noreferrer">
+      <a target="_blank" href={props.row.original.permalink!} rel="noreferrer">
         {props.getValue()}
       </a>
     ),
@@ -147,7 +137,7 @@ const COLUMNS = [
   columnHelper.accessor("conclusion", {
     cell: (props) => {
       const conclusion =
-        (props.row.getValue("conclusion") as Conclusion) || "in_progress";
+        (props.row.getValue("conclusion") as Conclusion) || "IN_PROGRESS";
 
       return (
         <span>
@@ -159,7 +149,7 @@ const COLUMNS = [
     },
     header: "Status",
   }),
-  columnHelper.accessor("started_at", {
+  columnHelper.accessor("startedAt", {
     header: "Started At",
   }),
   columnHelper.accessor("duration", {
@@ -167,7 +157,7 @@ const COLUMNS = [
       humanizeDuration(props.getValue(), { conjunction: " and ", largest: 2 }),
     header: "Duration",
   }),
-  columnHelper.accessor("completed_at", {
+  columnHelper.accessor("completedAt", {
     header: "Completed At",
   }),
 ];
@@ -192,7 +182,7 @@ export function ErrorBoundary({ error }: { error: Error }) {
 export default function Index() {
   const { statuses, pr, progress } = useLoaderDataReloading<typeof loader>();
 
-  const table: StandardTableOptions<Item> = {
+  const table: StandardTableOptions<SerializeFrom<Item>> = {
     data: statuses,
     columns: COLUMNS,
   };
@@ -210,8 +200,8 @@ export default function Index() {
       {
         <>
           <Header.Item>
-            <Header.Link target="_blank" href={pr._links.html.href}>
-              {pr.title}&nbsp;
+            <Header.Link target="_blank" href={pr!.permalink}>
+              {pr!.title}&nbsp;
               <LinkExternalIcon />
             </Header.Link>
           </Header.Item>
