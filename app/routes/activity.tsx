23c8dd0f7d252685a05b8d4be0c4675074de75cd
Elliana May <me@mause.me>
2023-04-06 07:08:16 +0000

    add basic activity display

--- app/routes/activity.tsx
+++ app/routes/activity.tsx
@@ -1,4 +1,9 @@
+import type { Octokit } from "@octokit/rest";
+import { Truncate } from "@primer/react";
+import { useLoaderData } from "@remix-run/react";
+import { createColumnHelper } from "@tanstack/react-table";
 import type { DataLoaderParams } from "~/components";
+import { StandardTable } from "~/components";
 import { getOctokit, getUser } from "~/octokit.server";
 
 export async function loader({ request }: DataLoaderParams<"">) {
@@ -9,4 +14,34 @@ export async function loader({ request }: DataLoaderParams<"">) {
   });
 
   return { events: events.data };
+}
+
+type Event = Awaited<
+  ReturnType<Octokit["activity"]["listEventsForAuthenticatedUser"]>
+>["data"][0];
+const columnHelper = createColumnHelper<Event>();
+
+export default function Activity() {
+  const { events } = useLoaderData<typeof loader>();
+
+  return (
+    <StandardTable
+      tableOptions={{
+        data: events,
+        columns: [
+          columnHelper.accessor("repo.name", { header: "Repository" }),
+          columnHelper.accessor("payload", {
+            header: "Event Payload",
+            cell: (props) => (
+              <Truncate title={props.row.original.type || "unknown"} expandable>
+                <code>
+                  <pre>{JSON.stringify(props.getValue(), undefined, 2)}</pre>
+                </code>
+              </Truncate>
+            ),
+          }),
+        ],
+      }}
+    />
+  );
 }
