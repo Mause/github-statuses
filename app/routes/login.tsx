67519a15b38bd593b0644dd524fa8ac6f385c563
Elliana May <me@mause.me>
2023-04-13 21:53:08 +0800

    autoredirect from login page

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,25 +1,22 @@
 import { Button } from "@primer/react";
 import type { DataFunctionArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import { Wrapper } from "~/components";
-import { getUserNoRedirect } from "~/octokit.server";
+import { authenticator } from "~/services/auth.server";
 
 export const loader = async ({ request }: DataFunctionArgs) =>
-  json({ user: await getUserNoRedirect(request) });
+  await authenticator().isAuthenticated(request, {
+    successRedirect: "/",
+  });
 
 export default function Login() {
-  const { user } = useLoaderData<typeof loader>();
+  useLoaderData<typeof loader>();
   return (
     <Wrapper>
       <></>
-      {user ? (
-        <div>{user.login} logged in</div>
-      ) : (
-        <Form action="/auth/github" method="post">
-          <Button type="submit">Login with GitHub</Button>
-        </Form>
-      )}
+      <Form action="/auth/github" method="post">
+        <Button type="submit">Login with GitHub</Button>
+      </Form>
     </Wrapper>
   );
 }
