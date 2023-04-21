921bd1612ac82fee59bea6a147735d0b50402b29
Elliana May <me@mause.me>
2023-04-21 04:10:12 +0000

    add empty page message

--- app/routes/activity.tsx
+++ app/routes/activity.tsx
@@ -61,7 +61,9 @@ export default function Activity() {
             </li>
           ))}
         </ul>
-        <StandardTable tableOptions={tableOptions} />
+        <StandardTable tableOptions={tableOptions}>
+          No pull requests found
+        </StandardTable>
       </>
     </Wrapper>
   );
