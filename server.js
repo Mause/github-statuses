1eb666c2f05598984d41c4023bff8a8e1dceb160
Elliana May <me@mause.me>
2023-05-02 05:36:13 +0000

    add handler level timeout

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import { createRequestHandler } from "@remix-run/vercel";
 import * as build from "@remix-run/dev/server-build";
+import { Response } from "@remix-run/node";
 
-export default createRequestHandler({
+const TIMEOUT = 7000;
+
+const handle = createRequestHandler({
   build,
   mode: process.env.NODE_ENV,
   getLoadContext() {
@@ -9,3 +12,13 @@ export default createRequestHandler({
     return {};
   },
 });
+export default async function (req, res) {
+  let timeoutId;
+  await Promise.race([
+    await handle(req, res),
+    new Promise(
+      (resolve) => (timeoutId = setTimeout(resolve(new Response(502)), TIMEOUT))
+    ),
+  ]);
+  clearTimeout(timeoutId);
+}
