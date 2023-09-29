8acd4476a0613d9d48026b747e5cadc848061456
Elliana May <me@mause.me>
2023-09-29 09:19:51 +0000

    feat: add index page

---
+++ app/routes/$owner/$repo.tsx
@@ -0,0 +1,15 @@
+import { Link } from "@remix-run/react";
+import { NavList } from "@primer/react";
+
+export default function Repo() {
+  return (
+    <NavList>
+      <NavList.Item as={Link} relative="route" to="./dashboard">
+        Dashboard
+      </NavList.Item>
+      <NavList.Item as={Link} relative="route" to="./pulls">
+        Pulls
+      </NavList.Item>
+    </NavList>
+  );
+}
