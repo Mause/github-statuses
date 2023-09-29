fb8f745abe6051a11df51aa4d2af7a58c8fcef81
Elliana May <me@mause.me>
2023-09-29 18:15:58 +0800

    fix: add outlet

--- app/routes/$owner/$repo.tsx
+++ app/routes/$owner/$repo.tsx
@@ -1,15 +1,18 @@
-import { Link } from "@remix-run/react";
+import { Link, Outlet } from "@remix-run/react";
 import { NavList } from "@primer/react";
 
 export default function Repo() {
   return (
-    <NavList>
-      <NavList.Item as={Link} relative="route" to="./dashboard">
-        Dashboard
-      </NavList.Item>
-      <NavList.Item as={Link} relative="route" to="./pulls">
-        Pulls
-      </NavList.Item>
-    </NavList>
+    <>
+      <NavList>
+        <NavList.Item as={Link} relative="route" to="./dashboard">
+          Dashboard
+        </NavList.Item>
+        <NavList.Item as={Link} relative="route" to="./pulls">
+          Pulls
+        </NavList.Item>
+      </NavList>
+      <Outlet />
+    </>
   );
 }
