80feb41b69f894b68716aaa87544f0e128e51b40
Elliana May <me@mause.me>
2023-10-01 12:36:41 +0800

    fix: clear the cookie!

--- app/routes/auth/github/callback.tsx
+++ app/routes/auth/github/callback.tsx
@@ -1,11 +1,16 @@
 // app/routes/auth/github/callback.tsx
-import type { LoaderArgs } from "@remix-run/node";
+import type { LoaderArgs} from "@remix-run/node";
+import { redirect } from "@remix-run/node";
 import { authenticator } from "~/services/auth.server";
-import { getRedirect } from "~/octokit.server";
+import { getRedirect, userPrefs } from "~/octokit.server";
 
 export async function loader({ request }: LoaderArgs) {
-  return authenticator().authenticate("github", request, {
-    successRedirect: await getRedirect(request),
+  await authenticator().authenticate("github", request, {
     failureRedirect: "/login",
+  });
+  const target = await getRedirect(request);
+
+  throw redirect(target, {
+    headers: { "Set-Cookie": await userPrefs.serialize(undefined) },
   });
 }
