ac367dbc9fbf599e667d5cb9639c7b3595c93148
Elliana May <me@mause.me>
2023-10-02 04:16:40 +0000

    catch and display import errors

--- test/whatever.test.tsx
+++ test/whatever.test.tsx
@@ -1,7 +1,13 @@
 import { screen, render } from "@testing-library/react";
 
 test("hello", async () => {
-  const {default: BasePage} = await import("~/routes/index");
+  let BasePage;
+  try {
+    BasePage = (await import("~/routes/index")).default;
+  } catch (e) {
+    console.error(e)
+  }
+  console.log(BasePage);
   const { createRemixStub } = await import("@remix-run/testing/dist/create-remix-stub");
 
   console.log({BasePage, createRemixStub})
