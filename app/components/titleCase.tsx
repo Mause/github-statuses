b14dd633dd135952e318981c21d8d80cd553b871
Elliana May <me@mause.me>
2023-11-14 08:51:01 +0000

    fix: title on dashes too

--- app/components/titleCase.tsx
+++ app/components/titleCase.tsx
@@ -1,5 +1,7 @@
 export function titleCase(conclusion: string) {
-  if (!conclusion) return conclusion;
-  const c = conclusion.toLowerCase().split("_").join(" ");
+  if (!conclusion) {
+    return conclusion;
+  }
+  const c = conclusion.toLowerCase().split(/[_-]/).join(" ");
   return c.slice(0, 1).toUpperCase() + c.slice(1);
 }
