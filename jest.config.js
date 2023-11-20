a1ba3cd7b9b062e35d8d481ea8a2ff55b2c1af76
Elliana May <me@mause.me>
2023-11-20 18:22:38 +0800

    module name mapper

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  moduleNameMapper: {
+    '.*\\.css$': '<rootDir>/test/CSSStub.js',
+    '^~/(.*)$': '<rootDir>/app/$1',
+  },
   testEnvironment: 'node',
 };
