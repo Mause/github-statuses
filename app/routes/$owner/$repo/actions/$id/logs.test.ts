1d62e7c6254ccac98ffcec5b85dcf2d157fa95d4
Elliana May <me@mause.me>
2023-11-20 07:34:14 +0000

    test other form

--- app/routes/$owner/$repo/actions/$id/logs.test.ts
+++ app/routes/$owner/$repo/actions/$id/logs.test.ts
@@ -1,18 +1,38 @@
 import { constructLine } from "./logs";
 
-test("basic", () => {
-  const res = constructLine(
-    "##vso[task.setvariable variable=testvar;]testvalue",
-  );
+describe("constructLine", () => {
+  test("vso", () => {
+    runTest("##vso[task.setvariable variable=testvar;]testvalue");
+  });
+  test("error", () => {
+    const el = constructLine("##[error]Error message");
+
+    if (typeof el === "object") {
+      expect(el.type).toEqual("span");
+      expect(el.props).toEqual({
+        children: "Error message",
+        style: {
+          color: "#ff5353",
+        },
+      });
+      expect(el.key).toEqual(null);
+    } else {
+      fail();
+    }
+  });
+});
+
+function runTest(inputString: string) {
+  const res = constructLine(inputString);
 
   if (typeof res === "string") {
     expect(JSON.parse(res)).toEqual({
       isVSO: true,
       directive: "task.setvariable variable=testvar;",
       line: "testvalue",
-      original: "##vso[task.setvariable variable=testvar;]testvalue",
+      original: inputString,
     });
   } else {
-    fail();
+    fail(inputString);
   }
-});
+}
