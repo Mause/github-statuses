850806150b6ab5b0d7c8f3276c46b9233679b337
Elliana May <me@mause.me>
2023-11-26 16:27:38 +0800

    add installation test route

---
+++ app/routes/installation.tsx
@@ -0,0 +1,91 @@
+import type { DataFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { getAppOctokit } from "../services/github-auth-auth.server";
+import { getUser, octokitFromToken } from "~/octokit.server";
+import { sessionStorage } from "~/services/session.server";
+import type { Octokit } from "@octokit/rest";
+import _ from "lodash";
+import AdmZip from "adm-zip";
+
+const INSTALLATION_ID = "installation_id";
+
+export const loader = async ({ request }: DataFunctionArgs) => {
+  const appOctokit = await getAppOctokit();
+
+  const installationId = await getInstallationId(request, appOctokit);
+  if (!installationId) {
+    return json({
+      error: "No installation found for this user",
+    });
+  }
+
+  const installationAccessToken =
+    await appOctokit.apps.createInstallationAccessToken({
+      installation_id: installationId,
+    });
+
+  const installationOctokit = octokitFromToken(
+    installationAccessToken.data.token,
+  );
+
+  const workflowRuns =
+    await installationOctokit.actions.listWorkflowRunsForRepo({
+      owner: "Mause",
+      repo: "duckdb",
+    });
+  const { workflow_runs } = workflowRuns.data;
+  const workflow_run =
+    workflow_runs.find((run) => run.name === "Windows") || workflow_runs[0];
+
+  const log_zip = await installationOctokit.request({
+    url: workflow_run.logs_url,
+    mediaType: {
+      format: "raw",
+    },
+  });
+
+  const zip = new AdmZip(Buffer.from(log_zip.data as ArrayBuffer));
+
+  const entries = _.groupBy(
+    zip
+      .getEntries()
+      .filter((entry) => !entry.isDirectory && entry.entryName.includes("/")),
+    (entry) => entry.entryName.split("/")[0],
+  );
+
+  return json({
+    installationAccessToken: installationAccessToken.data,
+    log_zip: _.map(entries, (entries, job) => [job, processEntries(entries)]),
+  });
+
+  function processEntries(entries: AdmZip.IZipEntry[]) {
+    return _.chain(entries)
+      .map((entry) => entry.name)
+      .sortBy((entry) => Number(entry.split("_")[0]))
+      .value();
+  }
+};
+
+async function getInstallationId(
+  request: Request,
+  appOctokit: Octokit,
+): Promise<number | undefined> {
+  const user = await getUser(request);
+
+  const session = await sessionStorage.getSession();
+
+  const installation_id = session.get(INSTALLATION_ID);
+  if (installation_id) return installation_id;
+
+  const { data: installations } = await appOctokit.apps.listInstallations();
+
+  const installation = installations.find(
+    (installation) => installation?.account?.login === user.login,
+  );
+
+  if (installation) {
+    session.set(INSTALLATION_ID, installation.id);
+    await sessionStorage.commitSession(session);
+    return installation.id;
+  }
+}
