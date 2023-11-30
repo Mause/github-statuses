dca6d88bee8d133c71d335e9b281573fdf9bb87c
Elliana May <me@mause.me>
2023-11-30 20:10:54 +0800

    refactor: switch to TabNavLink component

--- app/routes/storybook/dashboard.tsx
+++ app/routes/storybook/dashboard.tsx
@@ -1,20 +1,14 @@
 import { TabNav } from "@primer/react";
 import { Outlet } from "@remix-run/react";
-import { Link } from "react-router-dom";
+import { TabNavLink } from "~/components/TabNavLink";
 
 export default function DashboardStorySelector() {
   return (
     <>
       <TabNav aria-label="Main">
-        <TabNav.Link as={Link} to="dashboard">
-          Dashboard
-        </TabNav.Link>
-        <TabNav.Link as={Link} to="empty">
-          Empty
-        </TabNav.Link>
-        <TabNav.Link as={Link} to="not_a_fork">
-          Not a fork
-        </TabNav.Link>
+        <TabNavLink to="dashboard">Dashboard</TabNavLink>
+        <TabNavLink to="empty">Empty</TabNavLink>
+        <TabNavLink to="not_a_fork">Not a fork</TabNavLink>
       </TabNav>
       <Outlet />
     </>
