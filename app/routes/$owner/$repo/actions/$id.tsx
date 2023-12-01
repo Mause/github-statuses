028c5fb78cd0aef5029d2e2991cf23d5eedd34f4
Elliana May <me@mause.me>
2023-12-01 03:47:43 +0000

    feat: named breadcrumbs

---
+++ app/routes/$owner/$repo/actions/$id.tsx
@@ -0,0 +1,23 @@
+import type { LoaderFunction} from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Outlet } from "@remix-run/react";
+import { getOctokit } from "~/octokit.server";
+
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const octokit = await getOctokit(request);
+
+  const res = await octokit.actions.getWorkflowRun({
+    owner: params.owner!,
+    repo: params.repo!,
+    run_id: Number(params.id!),
+    exclude_pull_requests: false,
+  });
+
+  return json({
+    name: res.data.name!,
+  });
+};
+
+export default function Action() {
+  return <Outlet />;
+}
