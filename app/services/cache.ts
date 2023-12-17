774690d940dce4dfcc2de57dc8ac2cf7127fb8c9
Elliana May <me@mause.me>
2023-12-17 09:16:35 +0800

    custom client config

--- app/services/cache.ts
+++ app/services/cache.ts
@@ -1,10 +1,26 @@
-import { kv } from "@vercel/kv";
+import { createClient } from "@vercel/kv";
 import type { StrategyOptions } from "@octokit/auth-app";
 import _ from "lodash";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 type Cache = StrategyOptions["cache"];
 
+function throwError(msg: string): never {
+  throw new Error(msg);
+}
+
 function getCache(): Cache {
+  const { env } = process;
+  const kv = createClient({
+    url:
+      env.UPSTASH_REDIS_REST_URL ??
+      throwError("Missing env var: UPSTASH_REDIS_REST_URL"),
+    token:
+      env.UPSTASH_REDIS_REST_TOKEN ??
+      throwError("Missing env var: UPSTASH_REDIS_REST_TOKEN"),
+  });
   return {
     // @ts-expect-error
     async get(key: string): Promise<string | undefined> {
