e0a7743a997b9d2190d60c91da6bed8487bfddc7
Elliana May <me@mause.me>
2023-12-18 10:48:13 +0800

    rework test

--- test/whatever.test.tsx
+++ test/whatever.test.tsx
@@ -1,25 +1,55 @@
 import { screen, render } from "@testing-library/react";
+import { unstable_createRemixStub as createRemixStub } from "@remix-run/testing";
+
+import { UNSAFE_RemixContext } from "@remix-run/react";
+import type {
+  DataRouteObject,
+  IndexRouteObject,
+  NonIndexRouteObject,
+} from "react-router-dom";
+import type { ActionFunction, LoaderFunction } from "@remix-run/server-runtime";
+import { json } from "@remix-run/node";
+import { useContext } from "react";
+
+interface StubIndexRouteObject
+  extends Omit<IndexRouteObject, "loader" | "action"> {
+  loader?: LoaderFunction;
+  action?: ActionFunction;
+}
+interface StubNonIndexRouteObject
+  extends Omit<NonIndexRouteObject, "loader" | "action"> {
+  loader?: LoaderFunction;
+  action?: ActionFunction;
+}
+type StubRouteObject = StubIndexRouteObject | StubNonIndexRouteObject;
+type StubDataRouteObject = StubRouteObject & {
+  children?: DataRouteObject[];
+  id: string;
+};
 
 test("hello", async () => {
-  let BasePage: React.FunctionComponent<{asChildRoute: boolean}>;
-  try {
-    BasePage = (await import("~/routes/index")).default;
-  } catch (e) {
-    console.error(e)
-  }
-  console.log(BasePage);
-  const { createRemixStub } = await import("@remix-run/testing/dist/create-remix-stub");
-
-  console.log({BasePage, createRemixStub})
+  // let BasePage: React.FunctionComponent<{ asChildRoute: boolean }>;
+  // try {
+  //   BasePage = (await import("~/routes/index")).default;
+  // } catch (e) {
+  //   console.error(e);
+  //   throw e;
+  // }
 
   const Stub = createRemixStub([
     {
-      Component: () => <BasePage asChildRoute={false} />,
-      loader: () => Promise.resolve({}),
-    },
+      // Component: () => <BasePage asChildRoute={false} />,
+      Component: () => {
+        useContext(UNSAFE_RemixContext);
+        return <div>hello</div>;
+      },
+      loader: async () => json({ repos: [] }),
+      path: "/",
+      id: "root",
+    } satisfies StubDataRouteObject,
   ]);
 
-  render(<Stub />);
+  render(<Stub initialEntries={["/"]} />);
 
-  await screen.findByText("Unexpected Application Error!");
+  await screen.findByText("hello");
 });
