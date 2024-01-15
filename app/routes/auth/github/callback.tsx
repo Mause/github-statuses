bf57d746c151b8eed15090464ba917115d130f12
Elliana May <me@mause.me>
2024-01-15 13:34:57 +0800

    handle errors

--- app/routes/auth/github/callback.tsx
+++ app/routes/auth/github/callback.tsx
@@ -1,11 +1,25 @@
 // app/routes/auth/github/callback.tsx
-import type { LoaderFunction } from "@remix-run/node";
+import { json, type LoaderFunction } from "@remix-run/node";
 import { REDIRECT_URL } from "~/components/queryParams";
 import { authenticator } from "~/services/auth.server";
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const successRedirect =
-    new URL(request.url).searchParams.get(REDIRECT_URL) ?? "/";
+  const searchParams = new URL(request.url).searchParams;
+
+  if (searchParams.has("error")) {
+    return json(
+      {
+        error: searchParams.get("error"),
+        error_description: searchParams.get("error_description"),
+        error_uri: searchParams.get("error_uri"),
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+
+  const successRedirect = searchParams.get(REDIRECT_URL) ?? "/";
 
   return authenticator().authenticate("github", request, {
     successRedirect,
