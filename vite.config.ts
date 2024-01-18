3a755271d5e9b8137aa946f347c37271e7c19ad1
Elliana May <me@mause.me>
2024-01-18 12:30:16 +0800

    basic vite cfg

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+
+import tsconfigPaths from "vite-tsconfig-paths";
+const { createRoutesFromFolders } = require("@remix-run/v1-route-convention");
+
+export default defineConfig({
+  plugins: [
+    remix({
+      // When running locally in development mode, we use the built in remix
+      // server. This does not understand the vercel lambda module format,
+      // so we default back to the standard build output.
+      ignoredRouteFiles: ["**/.*"],
+      // appDirectory: "app",
+      // assetsBuildDirectory: "public/build",
+      // serverBuildPath: "api/index.js",
+      // publicPath: "/build/",
+      serverModuleFormat: "cjs",
+      future: {},
+      routes(defineRoutes) {
+        return createRoutesFromFolders(defineRoutes);
+      },
+      browserNodeBuiltinsPolyfill: {
+        modules: {
+          url: true,
+        },
+      },
+    }),
+    tsconfigPaths(),
+  ],
+});
