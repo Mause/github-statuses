1f687ab2ebfcbb6d3bf6c943949f9fbb0d7c73d0
Elliana May <me@mause.me>
2024-01-18 09:54:59 +0800

    remove vestiges of jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  moduleNameMapper: {
-    ".*\\.css$": "<rootDir>/test/CSSStub.js",
-    "^~/(.*)$": "<rootDir>/app/$1",
-  },
-  testEnvironment: "node",
-};
