fcc4439119c1d6bde23be7c8444ba19ec1d23c22
Elliana May <me@mause.me>
2024-02-02 14:17:46 +0800

    add flash

--- app/octokit.server.ts
+++ app/octokit.server.ts
@@ -9,6 +9,7 @@ import * as Sentry from "@sentry/remix";
 import type { RequestError } from "@octokit/request-error";
 import { GitHubAppAuthStrategy } from "./services/github-app-auth.server";
 import { getInstallationForLogin } from "~/services/installation";
+import { commitSession, getSession } from "./services/session.server";
 
 const Throttled = Octokit.plugin(throttling);
 
@@ -161,14 +162,7 @@ export async function call<Result, Variables extends RequestParameters>(
         console.error(e);
         if (isRequestError(e)) {
           if (e.message === "Bad credentials") {
-            throw new Response(
-              'Your session has expired. Please <a href="/login">login again</a>.',
-            );
-            /*
-          await authenticator().logout(request, {
-            redirectTo: "/",
-          });
-          */
+            await logoutAndRedirect(request);
           } else {
             console.log("Not a bad credentials error", e);
           }
@@ -179,6 +173,20 @@ export async function call<Result, Variables extends RequestParameters>(
       }
     },
   );
+}
+
+export async function logoutAndRedirect(request: Request) {
+  try {
+    await authenticator().logout(request, {
+      redirectTo: "/",
+    });
+  } catch (e) {
+    const res = e as Response;
+    const session = await getSession(request.headers.get("Cookie"));
+    session.flash("error", "Your session has expired");
+    res.headers.set("Set-Cookie", await commitSession(session));
+    throw e;
+  }
 }
 
 function isError(e: any): e is Error {
---
+++ test/auth.test.ts
@@ -0,0 +1,32 @@
+import { logoutAndRedirect } from "~/octokit.server";
+import { commitSession, getSession } from "~/services/session.server";
+
+describe("auth", () => {
+  it("logs out and redirects", async () => {
+    const request = {
+      headers: new Headers({
+        Cookie: await commitSession(await getSession()),
+      }),
+      url: new URL("http://localhost"),
+    } as unknown as Request;
+    expect(request.headers.get("Cookie")).toMatch(/_session/);
+
+    const response = await catchError<Response>(logoutAndRedirect(request));
+    expect(response.status).toBe(302);
+    expect(response.headers.get("Location")).toBe("/");
+
+    const session = await getSession(response.headers.getSetCookie()![0]);
+    expect(session.data).toEqual({
+      __flash_error__: "Your session has expired",
+    });
+  });
+});
+
+async function catchError<T>(fn: Promise<unknown>) {
+  try {
+    await fn;
+  } catch (e) {
+    return e as T;
+  }
+  throw new Error("Expected an error to be thrown.");
+}
