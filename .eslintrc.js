70c38a85d4b1eb9a041389c0b5fab070c9a182ff
Elliana May <me@mause.me>
2024-02-14 21:41:58 +0800

    disable some silly rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,28 +3,33 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript',
+    "plugin:react/recommended",
+    "standard-with-typescript",
     // "plugin:primer-react/recommended",
-    'eslint:recommended',
-    'plugin:@typescript-eslint/strict-type-checked'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
   ],
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: "detect",
+    },
+  },
+  rules: {
+    "@typescript-eslint/quotes": 0,
+    "@typescript-eslint/semi": 0,
+    "@typescript-eslint/comma-dangle": 0,
   },
   overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.eslint.json',
-    tsconfigRootDir: __dirname
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.eslint.json",
+    tsconfigRootDir: __dirname,
   },
-  parser: '@typescript-eslint/parser',
-  plugins: ['react', '@typescript-eslint'],
-  root: true
-}
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "@typescript-eslint"],
+  root: true,
+};
