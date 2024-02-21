df10e7ed5e50217d0820fc0d162644559b2994de
Elliana May <me@mause.me>
2024-02-22 01:23:59 +0800

    add logging plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import type { Plugin} from "vite";
 import { defineConfig } from "vite";
 
 import tsconfigPaths from "vite-tsconfig-paths";
 // import { createRoutesFromFolders } from "@remix-run/v1-route-convention";
 
+function op(): Plugin {
+  return {
+    name: "remix-op",
+    transform(code, id) {
+      console.log("transform", id);
+      return code;
+    },
+  };
+}
+
 export default defineConfig({
   plugins: [
+    op(),
     remix({
       // When running locally in development mode, we use the built in remix
       // server. This does not understand the vercel lambda module format,
