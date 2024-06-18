08e8193d996dbd0c5b581eba96198f5fc7d3655a
Elliana May <me@mause.me>
2024-06-18 17:40:59 +0800

    eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import react from "eslint-plugin-react";
+import { fixupPluginRules } from "@eslint/compat";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("@remix-run/eslint-config"), {
+    plugins: {
+        react: fixupPluginRules(react),
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+        },
+
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+
+    rules: {},
+}];
