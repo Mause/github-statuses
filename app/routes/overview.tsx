9f3b0ed2470537681713e69b0bb17c02c2611497
Elliana May <me@mause.me>
2024-06-23 14:55:45 +0800

    filler

--- app/routes/overview.tsx
+++ app/routes/overview.tsx
@@ -1,4 +1,42 @@
+import { call } from "~/services/octokit";
+import type { LoaderFunction } from "remix";
+import { useLoaderData } from "remix";
+import graphql from "graphql-tag";
+import { GetIssuesAndPullRequestsQuery } from "~/generated/graphql";
+
+export const GetIssuesAndPullRequests = graphql`
+  query GetIssuesAndPullRequests {
+    repository(owner: "octokit", name: "rest.js") {
+      issues(states: [OPEN], first: 10) {
+        nodes {
+          title
+          url
+        }
+      }
+      pullRequests(states: [OPEN], first: 10) {
+        nodes {
+          title
+          url
+        }
+      }
+    }
+  }
+`;
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const octokit = await call(request, GetIssuesAndPullRequestsQuery);
+
+  return {
+    issues: octokit.issues,
+    pullRequests: octokit.pullRequests,
+  };
+};
 
 export default function Overview() {
-  return 'Pull request and issues overview goes here';
+  const { issues, pullRequests } = useLoaderData<typeof loader>();
+  return (
+    <p>
+      {pullRequests.length} pull requests and {issues.length} issues
+    </p>
+  );
 }
