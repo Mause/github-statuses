3421175e9a76703dbeaf395b380b04f6cd6ac43b
Elliana May <me@mause.me>
2024-06-28 01:43:40 +0800

    use process.env

--- app/instrumentation.server.ts
+++ app/instrumentation.server.ts
@@ -2,7 +2,7 @@ import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import * as Sentry from "@sentry/remix";
 
 Sentry.init({
-  dsn: (window as any).ENV.SENTRY_DSN,
+  dsn: process.env.SENTRY_DSN,
   integrations: [nodeProfilingIntegration()],
   tracesSampleRate: 1,
   autoInstrumentRemix: true,
