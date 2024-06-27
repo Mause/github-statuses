9a1d68efb3d496622e2feee1942713cc46bd9a51
Elliana May <me@mause.me>
2024-06-28 02:08:40 +0800

    correct function

--- app/instrumentation.server.ts
+++ app/instrumentation.server.ts
@@ -1,6 +1,7 @@
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import * as Sentry from "@sentry/remix";
 
+console.log("Sentry is starting...");
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   integrations: [nodeProfilingIntegration()],
