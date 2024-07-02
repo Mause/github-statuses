336b577a880c494300ab6b2c1f9bee264365514e
Elliana May <me@mause.me>
2024-07-02 23:13:13 +0800

    improve things somewhat

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import type { Plugin } from "vite";
 import { defineConfig } from "vite";
 import { envOnlyMacros } from "vite-env-only";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 import tsconfigPaths from "vite-tsconfig-paths";
 import { createRoutesFromFolders } from "@remix-run/v1-route-convention";
@@ -19,6 +20,7 @@ function op(): Plugin {
 export default defineConfig({
   plugins: [
     op(),
+    nodePolyfills(),
     envOnlyMacros(),
     remix({
       // When running locally in development mode, we use the built in remix
