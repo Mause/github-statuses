9621f5c9be2e98cc214aa5f9b32a084d5fdbdab2
Elliana May <me@mause.me>
2024-08-26 18:12:57 +0800

    tweak

--- netlify/functions/hello-world/hello-world.ts
+++ netlify/functions/hello-world/hello-world.ts
@@ -4,12 +4,14 @@ export const config: Config = {
   path: "/*",
 };
 
-const handler: Handler = async (event, context) => {
-  const { name = "stranger" } = event.queryStringParameters || {};
+const handler: Handler = async (event: Request, context) => {
+  const name = new URL(event.url).searchParams.get("name") || "stranger";
 
   return new Response(
     JSON.stringify({
       message: `Hello, ${name}!`,
+      url: event.url,
+      headers: Object.fromEntries(event.headers.entries()),
     }),
   );
 };
