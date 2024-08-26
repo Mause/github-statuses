46e05f3823f540abae68d5b30070cfcca75ebd9a
Elliana May <me@mause.me>
2024-08-26 21:44:19 +0800

    what do we have here

--- netlify/functions/hello-world/hello-world.ts
+++ netlify/functions/hello-world/hello-world.ts
@@ -1,4 +1,5 @@
 import type { Config, Handler } from "@netlify/functions";
+import * as fs from "fs/promises";
 
 export const config: Config = {
   path: "/*",
@@ -7,11 +8,19 @@ export const config: Config = {
 const handler: Handler = async (event: Request, context) => {
   const name = new URL(event.url).searchParams.get("name") || "stranger";
 
+  let files = null;
+  try {
+    files = await fs.readdir(".");
+  } catch (error) {
+    console.error(error);
+  }
+
   return new Response(
     JSON.stringify({
       message: `Hello, ${name}!`,
       url: event.url,
       headers: Object.fromEntries(event.headers.entries()),
+      files,
     }),
     {
       headers: {
