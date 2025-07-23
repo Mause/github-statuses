38140ee1c6a397ffbf768affc103a49f10e858fe
Elliana May <me@mause.me>
2025-07-23 16:57:39 +0800

    merge files

--- instrumentation.server.mjs
+++ instrumentation.server.mjs
@@ -1,9 +1,12 @@
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import * as Sentry from "@sentry/remix";
 import { registerOTel } from '@vercel/otel';
  
 registerOTel({ serviceName: 'github-statuses' });
 
+console.log("Sentry is starting...");
 Sentry.init({
-  dsn: "https://fb814bfc76ad4564ab13b629de969706@o337133.ingest.us.sentry.io/4504964814929920",
+  dsn: process.env.SENTRY_DSN,
+  integrations: [nodeProfilingIntegration()],
   tracesSampleRate: 1,
 });
