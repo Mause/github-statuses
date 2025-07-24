395a0f0d0489fda70f26fa46c1069ef7a4e6f60e
Elliana May <me@mause.me>
2025-07-25 00:18:06 +0800

    build urls

--- app/routes/debug/index.tsx
+++ app/routes/debug/index.tsx
@@ -1,4 +1,5 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
+import { getRootURL } from "~/octokit.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   let paths: string[] | null = ["inaccessible"];
@@ -7,7 +8,15 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   } catch {}
 
   return {
-    manual: ["env", "kv", "redirect", "rootURL", "sentry", "user", "userExtra"],
+    manual: [
+      "env",
+      "kv",
+      "redirect",
+      "rootURL",
+      "sentry",
+      "user",
+      "userExtra",
+    ].map((name) => `${getRootURL()}/debug/${name}`),
     paths,
   };
 };
